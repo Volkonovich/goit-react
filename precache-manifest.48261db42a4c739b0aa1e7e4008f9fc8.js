869ebdbfe41fa965daffd05be2065c1bee6c53a6
VolkonovichD <volkonovich1989@gmail.com>
2019-12-18 12:00:28 +0200

    Updates

---
+++ precache-manifest.48261db42a4c739b0aa1e7e4008f9fc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6cb37041c110fe1ce55b2f8007a6fac",
+    "url": "/goit-react/./index.html"
+  },
+  {
+    "revision": "3bd92262d2abd1397990",
+    "url": "/goit-react/./static/css/main.fe34a908.chunk.css"
+  },
+  {
+    "revision": "c256645df2f531a6546a",
+    "url": "/goit-react/./static/js/2.2e7bdf50.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react/./static/js/2.2e7bdf50.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3bd92262d2abd1397990",
+    "url": "/goit-react/./static/js/main.7b237814.chunk.js"
+  },
+  {
+    "revision": "23a3d1aa165bd2c95fea",
+    "url": "/goit-react/./static/js/runtime-main.6f772e15.js"
+  }
+]);
