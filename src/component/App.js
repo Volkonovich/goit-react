3d9922ff14fcbe1c7b6e5bcadd54f04e9854b78b
VolkonovichD <volkonovich1989@gmail.com>
2020-01-17 14:21:14 +0200

    dz3

--- src/component/App.js
+++ src/component/App.js
@@ -2,8 +2,7 @@ import React from "react";
 
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
-
+const App = () => {
+  return <div className="App">{/* <Phonebook /> */}</div>;
+};
 export default App;
