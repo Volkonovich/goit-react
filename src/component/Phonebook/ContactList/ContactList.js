c64b842437f32973e6a781f1db04a90dcef64999
VolkonovichD <volkonovich1989@gmail.com>
2020-01-23 13:41:18 +0200

    new_dz

--- src/component/Phonebook/ContactList/ContactList.js
+++ src/component/Phonebook/ContactList/ContactList.js
@@ -1,11 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 const ContactList = ({ contacts = [], deleteContacts }) => {
-  // console.log("deleteContacts", deleteContacts);
   return (
     <>
-      {contacts.map(item => (
-        <div key={item.id}>
+      {contacts.map((item, indx) => (
+        <div key={indx}>
+          {console.log("object", item)}
           <p>{item.name}</p>
           <p>{item.number}</p>
           <button type="button" onClick={() => deleteContacts(item.id)}>
@@ -18,10 +18,10 @@ const ContactList = ({ contacts = [], deleteContacts }) => {
 };
 export default ContactList;
 
-ContactList.propTypes = {
-  contact: PropTypes.arrayOf(PropTypes.object),
-  id: PropTypes.string,
-  number: PropTypes.string,
-  name: PropTypes.string,
-  deleteContacts: PropTypes.func
-};
+// ContactList.propTypes = {
+//   contact: PropTypes.arrayOf(PropTypes.object),
+//   id: PropTypes.string,
+//   number: PropTypes.string,
+//   name: PropTypes.string,
+//   deleteContacts: PropTypes.func
+// };
