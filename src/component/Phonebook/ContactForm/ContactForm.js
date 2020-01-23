c1cd1f07a07a94dbb8652759fdf59b723542b738
VolkonovichD <volkonovich1989@gmail.com>
2020-01-23 14:00:26 +0200

    dz_3_s

--- src/component/Phonebook/ContactForm/ContactForm.js
+++ src/component/Phonebook/ContactForm/ContactForm.js
@@ -9,10 +9,10 @@ export default class ContactForm extends Component {
     name: "",
     number: "",
     contacts: [
-      // { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      // { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      // { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      // { id: "id-4", name: "Annie Copeland", number: "227-91-26" }
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" }
     ],
     filteredContacts: [],
     searchValue: ""
@@ -23,7 +23,7 @@ export default class ContactForm extends Component {
     // localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
     this.setState(prevState => {
       return {
-        contacts: [...prevState.contacts, local !== null ? local : {}]
+        contacts: [...prevState.contacts, local]
       };
     });
   }
