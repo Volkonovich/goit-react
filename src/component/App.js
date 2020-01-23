4878a11f05165c50876019d330b1febe66a2b827
VolkonovichD <volkonovich1989@gmail.com>
2020-01-23 12:17:13 +0200

    d3

--- src/component/App.js
+++ src/component/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Phonebook from "./Phonebook/Phonebook";
 import Gallery from "./Gallery/Gallary";
-// import css from "../component/Styles/styls.css";
 
 export default class App extends Component {
   state = {};
