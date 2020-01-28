1ec9338947b7388e4610c85f93b8ad629c0697f2
VolkonovichD <volkonovich1989@gmail.com>
2020-01-28 11:06:47 +0200

    Updates

---
+++ precache-manifest.b1f75e33197692d713e9a633f074c6bf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eebae196dca8a43075186c0e38b4b4a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b471ed1f0347576fb18",
+    "url": "/static/css/2.bc3aac6f.chunk.css"
+  },
+  {
+    "revision": "1b471ed1f0347576fb18",
+    "url": "/static/js/2.a242c531.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.a242c531.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e266403ca7d92a27e158",
+    "url": "/static/js/3.1d3c3879.chunk.js"
+  },
+  {
+    "revision": "ecb9670524d59ad33991",
+    "url": "/static/js/4.ce014a0f.chunk.js"
+  },
+  {
+    "revision": "e7c1b4df1623b18eb09a",
+    "url": "/static/js/5.c92f8e4a.chunk.js"
+  },
+  {
+    "revision": "1aed54af11bd79d76f76",
+    "url": "/static/js/6.10175fbf.chunk.js"
+  },
+  {
+    "revision": "ed7cd6c33e65520f1362",
+    "url": "/static/js/main.129a10a5.chunk.js"
+  },
+  {
+    "revision": "03cd784c559b3a831952",
+    "url": "/static/js/runtime-main.fbbdd8a5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react/precache-manifest.4df6b144f7d9ef23302b004b3e01e475.js"
+  "/precache-manifest.b1f75e33197692d713e9a633f074c6bf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
